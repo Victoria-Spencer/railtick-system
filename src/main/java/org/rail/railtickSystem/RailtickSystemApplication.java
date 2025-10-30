461046e9eaed88042113c5e63b4e4714d5d265bf
Frontier <1349615576@qq.com>
2025-10-30 21:09:41 +0800

    init

---
+++ src/main/java/org/rail/railtickSystem/RailtickSystemApplication.java
@@ -0,0 +1,13 @@
+package org.rail.railtickSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RailtickSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RailtickSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/rail/railtickSystem/RailtickSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rail.railtickSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RailtickSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
