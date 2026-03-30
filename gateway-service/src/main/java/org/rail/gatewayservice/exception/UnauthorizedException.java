7e844ee2f2f6a3fef36d0c110a5f656317009896
Victoria-Spencer <1557573258@qq.com>
2026-03-30 10:00:12 +0800

    no message

--- gateway-service/src/main/java/org/rail/gatewayservice/exception/UnauthorizedException.java
+++ gateway-service/src/main/java/org/rail/gatewayservice/exception/UnauthorizedException.java
@@ -5,6 +5,7 @@ package org.rail.gatewayservice.exception;
  */
 public class UnauthorizedException extends RuntimeException { // 继承RuntimeException，非受检异常
 
+    
     public UnauthorizedException() {
         super();
     }
